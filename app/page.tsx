54e696f879e31138dfccb291cd3d2f6a60607210
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-26 15:31:45 +0800

    start card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,46 @@
+"use client";
+
 import { Stack } from "@mui/material";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { useRouter } from "next/navigation";
+import { Fragment } from "react";
 
-function GenerateCard(title: string, subTitle: string, body: string) {
+function StartCard(props: {
+  title: string;
+  subTitle: string;
+  body: string;
+  route?: string;
+}) {
+  const router = useRouter();
   return (
-    <Card sx={{ width: 275 }}>
-      <CardContent>
-        <Typography sx={{ mb: 1.5 }} variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography variant="body1">{subTitle}</Typography>
-        <Typography color="text.secondary">{body}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Start</Button>
-      </CardActions>
-    </Card>
+    <Fragment>
+      <Card sx={{ width: 275 }}>
+        <CardContent>
+          <Typography sx={{ mb: 1.5 }} variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <Typography variant="body1">{props.subTitle}</Typography>
+          <Typography color="text.secondary">{props.body}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            disabled={!props.route}
+            onClick={() => {
+              if (props.route) {
+                router.push(props.route);
+              }
+            }}
+          >
+            Start
+          </Button>
+        </CardActions>
+      </Card>
+    </Fragment>
   );
 }
 
@@ -30,16 +52,18 @@ export default function Page() {
       justifyContent="center"
       alignItems="center"
     >
-      {GenerateCard(
-        "Ephemeral",
-        "Enter a room that uses sockets",
-        "Only participants of the same room will receive the message at the given moment in time"
-      )}
-      {GenerateCard(
-        "Persisted",
-        "Enter a room that uses firebase",
-        "Messages will be encrypted and stored in a firebase instance. Not yet available"
-      )}
+      <StartCard
+        title="Ephemeral"
+        subTitle="Enter a room that uses sockets"
+        body="Only participants of the same room will receive the message at the given moment in time"
+        route="/room"
+      ></StartCard>
+
+      <StartCard
+        title="Persisted"
+        subTitle="Enter a room that uses firebase"
+        body="Messages will be encrypted and stored in a firebase instance. Not yet available"
+      ></StartCard>
     </Stack>
   );
 }
