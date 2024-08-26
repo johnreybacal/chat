908fbb8812aa7238f8d951ec7fbcc21f18df9e84
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-26 18:56:40 +0800

    move export to out

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   reactStrictMode: true,
   basePath: "/chat",
+  images: {
+    unoptimized: true,
+  },
+  distDir: "out"
 };
 
 export default nextConfig;
